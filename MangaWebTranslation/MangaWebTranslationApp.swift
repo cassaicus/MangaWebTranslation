998fb3ea4c0dd18127f477c730cfe7b743b9833f
ibis <cassaicus+x@gmail.com>
2025-10-29 15:26:50 +0900

    Initial Commit

---
+++ MangaWebTranslation/MangaWebTranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaWebTranslationApp.swift
+//  MangaWebTranslation
+//
+//  Created by ibis on 2025/10/29.
+//
+
+import SwiftUI
+
+@main
+struct MangaWebTranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
