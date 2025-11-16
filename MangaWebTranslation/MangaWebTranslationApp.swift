985d53ad88ac0e62f7ec9701bc45e06af9c8ee5b
ibis <cassaicus+x@gmail.com>
2025-11-16 10:57:06 +0900

    3rd

--- MangaWebTranslation/MangaWebTranslationApp.swift
+++ MangaWebTranslation/MangaWebTranslationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MangaWebTranslationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CrossPlatformWebView()
         }
     }
 }
